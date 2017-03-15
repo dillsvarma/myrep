b357a99aa515f6768c1f8098555db904b971d911
dillsvarma <dileep.psdk@gmail.com>
2017-03-14 23:18:57 -0700

    Update testgr.groovy

--- testgr.groovy
+++ testgr.groovy
@@ -1,8 +1,12 @@
-stage {
-    scm {
-        git('https://github.com/dillsvarma/thisone.git')
-    }
-    build {
-        maven('clean package')
-    }
-}
+node('local') {
+git url: 'https://github.com/dillsvarma/thisone.git'
+stage '\u2776 Stage 1'
+echo "\u2600 stage 1 entering"
+ 
+def workspace = pwd()
+echo "\u2600 workspace=${workspace}"
+ 
+stage '\u2777 Stage 2'
+echo "\u2600 stage 1 entering"
+ sh "/usr/share/maven/bin/mvn clean package"
+} // node
